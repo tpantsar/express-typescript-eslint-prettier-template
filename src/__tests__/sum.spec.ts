dd289729d7145efd654788bd9a33bd1975fb2233
tpantsar <tomi.pantsar@gmail.com>
2025-03-19 11:21:40 +0200

    update tsconfig

--- src/__tests__/sum.spec.ts
+++ src/__tests__/sum.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 
-import { sum } from "../utils/sum";
+import { sum } from "../utils/sum.js";
 
 describe("sum function", () => {
   it("should add two positive numbers correctly", () => {
